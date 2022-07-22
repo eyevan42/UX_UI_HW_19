071f00483a2fe461e59d2dca09d81e6cc7ef107e
eyevan42 <100323503+eyevan42@users.noreply.github.com>
2022-07-21 20:41:30 -0500

    hotjar2
    update

--- js/index.js
+++ js/index.js
@@ -3,3 +3,11 @@ $("#img1").click(function(){
     rotateDegrees +=90;
     $("#img1 img").css("transform","rotate("+ rotateDegrees +"deg)");
 });
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3077460,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
